250c6bde60f673ad299cb05f336f82c7afd56abc
Jacob Emcken <jacob@emcken.dk>
2025-01-02 20:41:25 +0100

    Fix test cases on a non English system (en-US)

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [instaparse "1.4.9"]]
+  :jvm-opts ["-Duser.language=en_US"]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :sign-releases false}]])
